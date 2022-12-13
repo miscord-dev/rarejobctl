969c08280bace705125be4ee1366eed74ca74113
Kotaro Inoue <k.musaino@gmail.com>
2022-12-13 22:11:06 +0900

    Fix bug
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- cmd/rarejobctl/main.go
+++ cmd/rarejobctl/main.go
@@ -80,11 +80,13 @@ func main() {
 	logger.Info("start reserving tutor", zap.Int("year", *year), zap.Int("month", *month), zap.Int("day", *day), zap.String("time", *t))
 	for attempt := 0; attempt < maxRetryReservation; attempt++ {
 		r, err = rc.ReserveTutor(context.TODO(), from, margin)
+		if r != nil {
+			break
+		}
 		if err != nil {
 			logger.Warn("failed to reserve tutor. retrying...", zap.Error(err))
 		}
 	}
-
 	if err != nil {
 		api.PostMessage(os.Getenv("SLACK_CHANNEL"), slack.MsgOptionText("something went wrong... I failed to reserve your tutor. try again later.", false), slack.MsgOptionAsUser(true))
 		logger.Fatal("failed to reserve tutor", zap.Error(err))
